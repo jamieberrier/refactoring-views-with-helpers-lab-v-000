90ab64fb39ddf01b64499153ff43a2420e29acc5
Jamie Berrier <berrier.jamie@gmail.com>
2019-11-25 18:42:13 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,7 +2,7 @@ module SongsHelper
 
   def display_artist
     # If an artist is already associated with the song, return a link to the artist's show page
-    binding.pry
+    #binding.pry
     if @song.artist
       link_to @song.artist_name, artist_path(@song.artist)
     else # return a link to the song's edit page, with a link text of "Add Artist"
